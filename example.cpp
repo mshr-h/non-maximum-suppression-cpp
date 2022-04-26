95354fa6c8c48fc37d823a25ded916b8203eb680
Masahiro Hiramori <mhg00g13@gmail.com>
2022-04-26 20:05:34 +0900

    add threshold trackbar

--- example.cpp
+++ example.cpp
@@ -3,34 +3,47 @@
 
 #include <vector>
 #include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
 
 #include "utils.hpp"
 #include "nms.hpp"
+
+int threshold_max = 100;
+std::vector<std::vector<float> > rectangles =
+{
+  {300, 300, 400, 400},
+  {320, 320, 420, 420},
+  {295, 259, 415, 415},
+  {100, 100, 150, 150},
+  {90,  90,  180, 180},
+  {112, 112, 170, 170}
+};
+
+static void on_trackbar(int threshold_percent, void *)
+{
+  cv::Mat imgAfter(cv::Size(500, 500), cv::DataType<float>::type);
+  float threshold = (float) threshold_percent / threshold_max;
+  std::vector<cv::Rect> reducedRectangle = nms(rectangles, threshold);
+  DrawRectangles(imgAfter, reducedRectangle);
+  cv::imshow("After", imgAfter);
+}
 
 int main()
 {
   // initialization
   cv::Mat imgBefore(cv::Size(500, 500), cv::DataType<float>::type);
-  cv::Mat imgAfter = imgBefore.clone();
-  float threshold	= 0.5;
-  std::vector<std::vector<float> > rectangles =
-  {
-    {300, 300, 400, 400},
-    {320, 320, 420, 420},
-    {295, 259, 415, 415},
-    {100, 100, 150, 150},
-    {90,  90,  180, 180},
-    {112, 112, 170, 170}
-  };
+  int threshold_percent = 10;
 
   // before
   DrawRectangles(imgBefore, rectangles);
   cv::imshow("Before", imgBefore);
+
+  cv::namedWindow("After", cv::WINDOW_AUTOSIZE);
+  char TrackbarName[50];
+  sprintf( TrackbarName, "Threshold (%%)");
+  cv::createTrackbar( TrackbarName, "After", &threshold_percent, threshold_max, on_trackbar );
   
   // after
-  std::vector<cv::Rect> reducedRectangle = nms(rectangles, threshold);
-  DrawRectangles(imgAfter, reducedRectangle);
-  cv::imshow("After", imgAfter);
   
   cv::waitKey(0);
 }
